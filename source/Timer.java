04c7f46c17be5e279da17003986551467cb929eb
alu0100887686 <alu0100887686@ull.edu.es>
2017-03-15 19:32:03 +0000

    Timer.java

---
+++ source/Timer.java
@@ -0,0 +1,43 @@
+
+public final class Timer {
+
+	// Beginning and end of the period
+	private static long start, end;
+
+	public Timer(){
+		start = 0;
+		end   = 0;
+	}
+
+	/** Start trigger */
+	public static void start(){
+		setStart(System.currentTimeMillis());
+	}
+
+	/** End trigger */
+	public static void end(){
+		setEnd(System.currentTimeMillis());
+	}
+
+	/** Elapsed Time */
+	public static long elapsedTime(){
+		return getEnd() - getStart();
+	}
+
+	//Getter and setters
+	public static long getStart() {
+		return start;
+	}
+
+	public static void setStart(long start) {
+		Timer.start = start;
+	}
+
+	public static long getEnd() {
+		return end;
+	}
+
+	public static void setEnd(long end) {
+		Timer.end = end;
+	}
+}
